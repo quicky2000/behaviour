8e612bf411b0d921daf8d1de28af435d7dbc1ea8
Julien Thevenon <julien_thevenon@yahoo.fr>
2014-12-10 17:44:33 +0100

    Add licence and header

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+/*    This file is part of behaviour
+      Copyright (C) 2014  Julien Thevenon ( julien_thevenon at yahoo.fr )
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 #include "simple_gui.h"
 #include "random_behaviour.h"
 #include "asocial_behaviour.h"
@@ -138,7 +154,7 @@ int main(void)
       l_gui.setPixel(l_x,l_y,l_white);
       ++l_index;
     }
- l_index = 0;
+ l_index = 50;
   while(l_index < 0)
     {
       //      uint32_t l_x = (int)(l_width * ((float)rand())/RAND_MAX);
